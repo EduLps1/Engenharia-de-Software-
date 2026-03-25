3167ab821117a7fd19456968757549a87320b05c
Thalita Menezes <thalitateixeiramenezes2@gmail.com>
2026-03-25 05:15:22 -0300

    feat: implementacao da estrutura MVC para login

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,34 @@
 <?php
-
+session_start();
 
 require_once __DIR__ . '/../app/controllers/ReservaController.php';
+require_once __DIR__ . '/../app/controllers/LoginController.php';
 
-$controller = new ReservaController();
+$reservaController = new ReservaController();
+$loginController = new LoginController();
 
 $action = $_GET['action'] ?? 'listar';
 
 switch ($action) {
+   
+    case 'login':
+        $loginController->index(); 
+        break;
+
+    case 'autenticar':
+        $loginController->logar(); 
+        break;
 
     case 'criar':
-        $controller->criar();
+        $reservaController->criar();
         break;
 
     case 'cancelar':
-        $controller->cancelar();
+        $reservaController->cancelar();
         break;
 
     default:
-        $reservas = $controller->listar();
+        $reservas = $reservaController->listar();
         require __DIR__ . '/../app/views/listar.php';
+        break;
 }
